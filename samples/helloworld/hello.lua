aa47a7d1efe036cd00d68075bedbffea0ad9587b
Hisai Toru <toru@torus.jp>
2013-05-29 22:58:13 +0900

    できた

---
+++ samples/helloworld/hello.lua
@@ -0,0 +1,3 @@
+function greeting()
+   print "Hello World!"
+end
--- samples/helloworld/helloworld.cpp
+++ samples/helloworld/helloworld.cpp
@@ -4,10 +4,14 @@ int main(int argc, char **argv) {
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
 
-    if (luaL_loadfile(L, "hello.lua")) {
+    if (luaL_dofile(L, "hello.lua")) {
 	fprintf(stderr, "Lua Error: %s", lua_tostring(L, -1));
     }
 
     lua_getglobal(L, "greeting");
-    lua_call(L, 0, 0);
+    if (lua_pcall(L, 0, 0, 0)) {
+        fprintf(stderr, "Lua Error: %s\n", lua_tostring(L, -1));
+    }
+
+    return 0;
 }
