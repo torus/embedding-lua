3e672f1a72c17bc1fc4ddbfddc80b57762ab3b6b
Hisai Toru <toru@torus.jp>
2013-09-22 20:01:56 +0900

    added hello world on Xcode

---
+++ samples/xcode/HelloLua/HelloLua/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HelloLua
+//
+//  Created by Hisai Toru on 2013/09/22.
+//  Copyright (c) 2013年 Kronecker's Delta Studio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
